eba2ffd36adb4d65fdda25adce00ed77c1a3cad3
jedoGit <ajvil@xps17.com>
2023-12-24 09:46:43 -0500

    Fix controller spelling

--- REST-Service-Blog/REST-API-Server/routes/feed.js
+++ REST-Service-Blog/REST-API-Server/routes/feed.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const feedController = require("../contollers/feed");
+const feedController = require("../controllers/feed");
 const router = express.Router();
 
 //-----------------
