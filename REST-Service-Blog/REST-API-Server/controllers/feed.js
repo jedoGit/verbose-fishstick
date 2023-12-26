6e681bfb8b7a0ee0b03a0c4dc6b58a84e441ae82
jedoGit <ajvil@xps17.com>
2023-12-26 06:17:34 -0500

    Hook post controller with model

--- REST-Service-Blog/REST-API-Server/controllers/feed.js
+++ REST-Service-Blog/REST-API-Server/controllers/feed.js
@@ -1,4 +1,6 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post");
+
 //-----------------
 // Controller: getPosts
 //-----------------
@@ -28,12 +30,10 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect.",
+      errors: errors.array(),
+    });
   }
 
   const title = req.body.title;
@@ -42,14 +42,22 @@ exports.createPost = (req, res, next) => {
   console.log("CREATE POST");
 
   // Create post in db
-  res.status(201).json({
-    message: "Post Created Successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: "Jedo" },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/itunesImage.jpg",
+    creator: { name: "Jedo" },
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post Created Successfully!",
+        post: post,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- REST-Service-Blog/REST-API-Server/models/post.js
+++ REST-Service-Blog/REST-API-Server/models/post.js
@@ -6,7 +6,7 @@ const postSchema = new Schema(
     title: { type: String, required: true },
     imageUrl: { type: String, required: true },
     content: { type: String, required: true },
-    creator: { type: String, required: true },
+    creator: { type: Object, required: true },
   },
   { timestamps: true }
 );
