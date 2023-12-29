c10e2cb11a4c43d56256c42934a10f51ae3ebd38
jedoGit <ajvil@xps17.com>
2023-12-29 08:01:45 -0500

    Add Delete and Update post

--- REST-Service-Blog/REST-API-Client/src/pages/Feed/Feed.js
+++ REST-Service-Blog/REST-API-Client/src/pages/Feed/Feed.js
@@ -59,7 +59,12 @@ class Feed extends Component {
       })
       .then((resData) => {
         this.setState({
-          posts: resData.posts,
+          posts: resData.posts.map((post) => {
+            return {
+              ...post,
+              imagePath: post.imageUrl,
+            };
+          }),
           totalPosts: resData.totalItems,
           postsLoading: false,
         });
@@ -114,8 +119,10 @@ class Feed extends Component {
 
     let url = "http://localhost:8081/feed/post";
     let method = "POST";
+
     if (this.state.editPost) {
-      url = "URL";
+      url = "http://localhost:8081/feed/post/" + this.state.editPost._id;
+      method = "PUT";
     }
 
     fetch(url, {
@@ -172,7 +179,11 @@ class Feed extends Component {
 
   deletePostHandler = (postId) => {
     this.setState({ postsLoading: true });
-    fetch("URL")
+
+    let url = "http://localhost:8081/feed/post/" + postId;
+    let method = "DELETE";
+
+    fetch(url, { method: method })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Deleting a post failed!");
--- REST-Service-Blog/REST-API-Server/controllers/feed.js
+++ REST-Service-Blog/REST-API-Server/controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
@@ -80,6 +82,9 @@ exports.createPost = (req, res, next) => {
 //-----------------
 exports.getPost = (req, res, next) => {
   const postId = req.params.postId;
+
+  console.log("GET POST");
+
   Post.findById(postId)
     .then((post) => {
       if (!post) {
@@ -98,4 +103,118 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+//-----------------
+// Controller: updatePost
+//-----------------
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const postId = req.params.postId;
+  const title = req.body.title;
+  const content = req.body.content;
+
+  let imageUrl = req.body.image;
+
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\", "/");
+  }
+
+  if (!imageUrl) {
+    const error = new Error("No file selected.");
+    error.statusCode = 422;
+    throw error;
+  }
+
+  console.log("UPDATE POST");
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post with ID: " + postId);
+        statusCode = 404;
+        throw error;
+      }
+
+      // Delete the old image if there's a new image uploaded
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+
+      post.title = title;
+      post.content = content;
+      post.imageUrl = imageUrl;
+
+      return post.save();
+    })
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: "Post Updated Successfully.", post: result });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+//-----------------
+// Controller: deletePost
+//-----------------
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  console.log("DELETE POST");
+
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post with ID: " + postId);
+        statusCode = 404;
+        throw error;
+      }
+
+      // Delete the image
+      clearImage(post.imageUrl);
+
+      return Post.findByIdAndDelete(postId);
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post Deleted Successfully." });
+    })
+    .catch((err) => {
+      console.log(err);
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+//-----------------
+// Helper Functions
+//-----------------
+
+//-----------------
+// clearImage()
+//             Helper function when deleting/updating image
+//-----------------
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath).replace("\\", "/");
+  fs.unlink(filePath, (err) => {
+    if (!Object.is(err, null)) {
+      console.log("clearImage Error: " + err);
+    }
+  });
 };
