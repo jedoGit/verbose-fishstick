0ca88edd4d6104e29ae33b20bbef52c8589caf18
jedoGit <ajvil@xps17.com>
2024-01-06 07:23:17 -0500

    Fix front-end pagination

--- REST-Service-Blog/REST-API-Client/src/pages/Feed/Feed.js
+++ REST-Service-Blog/REST-API-Client/src/pages/Feed/Feed.js
@@ -168,19 +168,25 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
+          } else {
+            updatedTotalPosts++;
+            if (prevState.posts.length >= 2) {
+              updatedPosts.pop();
+            }
+            updatedPosts.unshift(post);
           }
           return {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           };
         });
       })
